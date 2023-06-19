9ce9a3c9eb517ae2ef3be09e74990ee70fe1cdb1
Jack Sari <janasarii@gmail.com>
2023-06-18 20:57:32 -0500

    add

--- controllers/flight.controller.js
+++ controllers/flight.controller.js
@@ -1,14 +1,147 @@
-import { getExample } from "../services/flight.service.js"
+import {
+  asientosOcupados,
+  getAirplaneByFlight,
+  getCheckFlight,
+  getTotalAsientosByFlightByAirplaneByType,
+  getUsuariosDisponiblesEconomico,
+  getUsuariosDisponiblesEconomicoPremium,
+  validarNumeros,
+} from "../services/flight.service.js";
 
 export const getFlightById = async (req, res) => {
-    const [rows,fields]= await getExample(req.params.id)
-    //fields tiene las cabeceras de la tabla
-    console.log('obteniendo Id del vuelo', req.params.id)
-    // console.log('data', data)
-    return res.status(200).json({
-        status: true,
-        message: 'la API funciona',
-        data: rows
-    })
-}
+  const flight_id = req.params.id;
 
+  // Valido el id del vuelo
+  if (!validarNumeros(req.params.id)) {
+    return res.status(404).json({
+      code: 404,
+      data: {},
+      // message: 'El id del vuelo debe ser un numero'
+    });
+  }
+
+  if (!(await getCheckFlight(flight_id))) {
+    return res.status(404).json({
+      code: 404,
+      data: {},
+    });
+  }
+
+  // Busqueda del avion
+  const [rowAirplane] = await getAirplaneByFlight(flight_id);
+
+  const airplane_id =
+    rowAirplane.length > 0 ? rowAirplane[0].airplane_id : null;
+
+  // Total de asientos Economica premium
+  let [rowsTotalEP, fields] = await getTotalAsientosByFlightByAirplaneByType(
+    flight_id,
+    airplane_id,
+    2
+  );
+
+  for (let index = 0; index < rowsTotalEP.length; index++) {
+    rowsTotalEP[index].idIndex = index;
+  }
+
+  //   // Pasajeros sin asignar en clase economica premium
+  let [rowsEconomicoPremium] = await getUsuariosDisponiblesEconomicoPremium(
+    flight_id
+  );
+
+  // Asientos ocupados
+  //   let [rowsOcupadosEP] = await asientosOcupados(flight_id, 2);
+  let rowsOcupadosEP = rowsTotalEP.filter((it) => it.boarding_pass_id);
+
+  // Simulacion
+
+  for (let index = 0; index < rowsTotalEP.length; index++) {
+    rowsTotalEP[index].idIndex = index;
+  }
+
+  // Filtros para economicos premium
+  for (let indexJ = 0; indexJ < rowsOcupadosEP.length; indexJ++) {
+    const elementOcupadoEP = rowsOcupadosEP[indexJ];
+    for (let indexY = 0; indexY < rowsEconomicoPremium.length; indexY++) {
+      const elementDisponible = rowsEconomicoPremium[indexY];
+      //   console.log('sd', elementDisponible.purchase_id);
+      //   console.log(
+      //     "pu",
+      //     elementOcupadoEP.purchase_id, elementDisponible.purchase_id
+      //   );
+      if (elementOcupadoEP.purchase_id == elementDisponible.purchase_id) {
+        rowsEconomicoPremium[indexY].encontro = true;
+        // console.log("encontroe");
+      }
+    }
+  }
+
+  for (let indexY = 0; indexY < rowsOcupadosEP.length; indexY++) {
+    const elementDisponible = rowsOcupadosEP[indexY];
+    for (let index = 0; index < rowsTotalEP.length; index++) {
+      const elementTotalEP = rowsTotalEP[index];
+      if (elementTotalEP.seat_id == elementDisponible.seat_id) {
+        const passengers = rowsEconomicoPremium.filter(
+          (it) => it.purchase_id == elementDisponible.purchase_id && it.encontro
+        );
+        let id = elementTotalEP.idIndex;
+        id++;
+        passengers.forEach((it) => {
+          rowsTotalEP[id].boarding_pass_id = it.boarding_pass_id;
+          rowsTotalEP[id].purchase_id = it.purchase_id;
+          id++;
+          //   it.boarding_pass_id =12222;
+          it.seat_id = rowsTotalEP[id].seat_id;
+          // console.log('seat', elementTotalEP.seat_id);
+        });
+      }
+    }
+  }
+
+  rowsEconomicoPremium = rowsEconomicoPremium.filter((it) => !it.seat_id);
+
+  for (let indexY = 0; indexY < rowsTotalEP.length; indexY++) {
+    const elementTotalEP = rowsTotalEP[indexY];
+    if (!elementTotalEP.boarding_pass_id) {
+      if (rowsEconomicoPremium[0]) {
+        rowsTotalEP[indexY].boarding_pass_id =
+          rowsEconomicoPremium[0].boarding_pass_id;
+        rowsTotalEP[indexY].purchase_id = rowsEconomicoPremium[0].purchase_id;
+
+        rowsEconomicoPremium.shift();
+      }
+    }
+  }
+
+  // Filtros para economicos
+
+  // Total de asientos Economica premium
+  let [rowsTotalE, fieldsE] = await getTotalAsientosByFlightByAirplaneByType(
+    flight_id,
+    airplane_id,
+    3
+  );
+
+  for (let index = 0; index < rowsTotalE.length; index++) {
+    rowsTotalE[index].idIndex = index;
+  }
+
+  // Pasajeros sin asignar en clase economica
+  let [rowsEconomico] = await getUsuariosDisponiblesEconomico(flight_id);
+
+  //   let [rowsOcupadosEP] = await asientosOcupados(flight_id, 2);
+  let rowsOcupadosE = rowsTotalEP.filter((it) => it.boarding_pass_id);
+
+  return res.status(200).json({
+    status: true,
+    message: "la API funciona",
+    data: {
+      rowsTotalEP,
+      //   rowsEconomicoPremium: rowsEconomicoPremium.filter((it) => it.encontro),
+      //   rowsEconomico,
+      //   rowsEconomicoPremium: rowsEconomicoPremium.filter((it) => !it.seat_id),
+      rowsOcupadosEP,
+      rowsTotalE,
+    },
+  });
+};
--- services/flight.service.js
+++ services/flight.service.js
@@ -1,27 +1,98 @@
 //todas las consultas para traer la info ordenada
-import { pool } from '../db.js'
-export const getExample = async (id) => {
-    const query = `
-        select 
-            bp.passenger_id passengerId,
-            p.dni,
-            p.name,
-            p.age,
-            p.country,
-            bp.boarding_pass_id boardingPassId,
-            bp.purchase_id purchaseId,
-            bp.seat_type_id seatTypeId,
-            bp.seat_id seatId, 
-            s.seat_column,
-            s.seat_row,
-            st.name
-        from boarding_pass bp
-        left join flight f on f.flight_id  = bp.flight_id
-        left join passenger p on p.passenger_id = bp.passenger_id 
-        left join seat s on s.seat_id = bp.seat_id 
-        left join seat_type st on st.seat_type_id = s.seat_type_id 
-        where bp.flight_id = ?;
-    `;
-    const params = [id];
-    return pool.execute(query, params)
+import { pool } from "../db.js";
+
+export function validarNumeros(texto) {
+  // Expresión regular que solo permite números
+  var regex = /^[0-9]+$/;
+
+  // Verificar si el texto cumple con el patrón
+  return regex.test(texto);
 }
+
+export const getCheckFlight = async (id) => {
+  const query = `
+    select f.flight_id  from flight f
+    where f.flight_id = ?;
+        `;
+  const params = [id];
+  const [rows] = await pool.execute(query, params);
+  console.log("rows", id, rows);
+  return rows.length > 0 ? true : false;
+};
+
+export const getAirplaneByFlight = async (id) => {
+  const query = `
+          select f.airplane_id from flight f
+          where f.flight_id = ?;
+          `;
+  const params = [id];
+  return pool.execute(query, params);
+};
+
+export const getTotalAsientosByFlightByAirplaneByType = async (
+  flight_id,
+  airplane_id,
+  seat_type_id
+) => {
+  const query = `
+    select s.seat_id, s.seat_column , s.seat_row, ps.boarding_pass_id, ps.purchase_id, st.name, bp2.seat_id seat_filter from seat s 
+    left join (
+        select 
+            bp.boarding_pass_id, bp.seat_id , s.seat_row, s.seat_column,  bp.purchase_id
+        from boarding_pass bp
+        left join flight f on f.flight_id = bp.flight_id 
+        left join seat s on s.seat_id = bp.seat_id 
+        where bp.flight_id = ? and s.seat_id is not null
+        order by s.seat_column asc 
+    ) ps on ps.seat_id = s.seat_id 
+    left join seat_type st on st.seat_type_id = s.seat_type_id 
+    left join boarding_pass bp2 on bp2.boarding_pass_id = ps.boarding_pass_id
+    where s.airplane_id = ? and st.seat_type_id = ?
+    order by s.seat_row asc, s.seat_id asc;
+      `;
+  const params = [flight_id, airplane_id, seat_type_id];
+  return pool.execute(query, params);
+};
+
+export const getUsuariosDisponiblesEconomicoPremium = async (id) => {
+  const query = `
+        select 
+            bp.*,
+            if( p.age < 18 , 1, 0) is_menor,
+            st.name 
+        from boarding_pass bp 
+        join passenger p on p.passenger_id = bp.passenger_id 
+        join seat_type st on st.seat_type_id = bp.seat_type_id 
+        where bp.seat_id is null and bp.flight_id = ? and st.seat_type_id = 2
+        order by st.seat_type_id asc, bp.purchase_id asc;
+    `;
+  const params = [id];
+  return pool.execute(query, params);
+};
+
+export const getUsuariosDisponiblesEconomico = async (id) => {
+  const query = `
+          select 
+              bp.*,
+              if( p.age < 18 , 1, 0) is_menor,
+              st.name 
+          from boarding_pass bp 
+          join passenger p on p.passenger_id = bp.passenger_id 
+          join seat_type st on st.seat_type_id = bp.seat_type_id 
+          where bp.seat_id is null and bp.flight_id = ? and st.seat_type_id = 3
+          order by st.seat_type_id asc, bp.purchase_id asc;
+      `;
+  const params = [id];
+  return pool.execute(query, params);
+};
+
+export const asientosOcupados = async (id, seat_type_id) => {
+  const query = `
+    select bp.*,  s.seat_column, s.seat_row, s.seat_type_id  from boarding_pass bp
+    join seat s on s.seat_id = bp.seat_id 
+    where bp.seat_id is not null and bp.flight_id = ? and s.seat_type_id = ?
+    order by s.seat_row asc, s.seat_id asc;
+`;
+  const params = [id, seat_type_id];
+  return pool.execute(query, params);
+};
